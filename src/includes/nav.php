4ee1c9e23d22677064cd6b5ef5896c4c43061971
Dylan <dylanv.dwt@hotmail.com>
2024-10-08 22:40:13 +0200

    Add burger menu

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -1,21 +1,28 @@
 <header>
-    <nav class="navbar bg-dark border-bottom border-body p-3" data-bs-theme="dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">Mon site</a>
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body p-3" data-bs-theme="dark">
+        <div class="container d-flex justify-content-between">
+            <a class="navbar-brand" href="#"><img src="assets/images/logo-heaj.png" alt=""></a>
 
-            <ul class="navbar-nav d-flex flex-row gap-5">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Présentation</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Fonctionnalités</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Trailer</a>
-                </li>
-            </ul>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <button class="btn btn-primary" id="toggleMode">Dark Mode</button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav mb-2 mb-lg-0 d-flex flex-lg-row gap-5">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Présentation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Fonctionnalités</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Trailer</a>
+                    </li>
+                    <li class="nav-item">
+                        <button class="btn btn-primary" id="toggleMode">Dark Mode</button>
+                    </li>
+                </ul>
+            </div>           
         </div>
     </nav>
 </header>
