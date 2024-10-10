fd07688112af22328de2dd76cc68004309751803
Dylan <dylanv.dwt@hotmail.com>
2024-10-10 22:45:16 +0200

    Remove Bootstrap

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,6 @@
 "use strict";
-console.clear();
 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { gsap } from "gsap";
     
@@ -9,22 +8,3 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 gsap.registerPlugin(ScrollTrigger,ScrollToPlugin);
-
-
-const toggleButton = document.getElementById('toggleMode');
-const htmlElement = document.documentElement;
-
-
-function toggleTheme(){
-    const currentTheme = htmlElement.getAttribute('data-bs-theme');
-
-    if(currentTheme === 'light'){
-        htmlElement.setAttribute('data-bs-theme', 'dark');
-        toggleButton.textContent = 'Light Mode';
-    } else{
-        htmlElement.setAttribute('data-bs-theme', 'light');
-        toggleButton.textContent = 'Dark Mode';
-    }
-}
-
-toggleButton.addEventListener('click', toggleTheme);
