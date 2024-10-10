77a4b60f4c817f827a55a8e8d56a37d49218e061
Dylan <dylanv.dwt@hotmail.com>
2024-10-10 22:55:43 +0200

    Add travel experiment

--- src/app.php
+++ src/app.php
@@ -2,7 +2,13 @@
 require 'header.php';
 ?>
 <main>
-
+    <section class="app">
+        <div class="app__character"></div>
+        <div class="app__ground"></div>
+        <div class="app__message">
+            <p>Salut ! Je m'appelle iLabrador et je vais vous expliquer ma routine de chien-guide.</p>
+        </div>
+    </section>
 </main>
 <?php
 require 'close.php';
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,4 +7,33 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-gsap.registerPlugin(ScrollTrigger,ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+
+let character = document.querySelector('.app__character');
+let message = document.querySelector('.app__message');
+let movementEnabled = true;
+let position = 0;
+
+document.addEventListener('keydown', function(event){
+    if(event.code === 'Space' && movementEnabled === true && position < 1200){
+        event.preventDefault();
+        position += 25;
+        character.style.left = `${position}px`;
+
+        if(position == 550){
+            message.classList.add('appear');
+            movementEnabled = false;
+        };
+
+        if(position == 575){
+            message.classList.remove('appear');
+        };
+    };
+});
+
+document.addEventListener('keyup', function(event){
+    if(event.code === 'Space' && movementEnabled === false){
+        movementEnabled = true;
+    };
+});
