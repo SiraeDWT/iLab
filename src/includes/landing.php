73ae464c137e568556d17c101f7959feaea154a0
Dylan <dylanv.dwt@hotmail.com>
2024-10-15 19:02:21 +0200

    Add intro content

--- src/includes/landing.php
+++ src/includes/landing.php
@@ -2,8 +2,11 @@
     <section class="intro">
         <div class="wrapper">
             <h1 class="intro__title title">iLabrador</h1>
-            <p class="intro__text"></p>
-            <img src="" alt="">
+            <p class="intro__text">Apprenez-en plus sur les difficultés rencontrées au quotidien par les personnes malvoyantes.</p>
+            <ul class="intro__list">
+                <li class="intro__el"><a href="#presentation" class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14.601 21.5c0 1.38-1.116 2.5-2.499 2.5-1.378 0-2.499-1.12-2.499-2.5s1.121-2.5 2.499-2.5c1.383 0 2.499 1.119 2.499 2.5zm-2.42-21.5c-4.029 0-7.06 2.693-7.06 8h3.955c0-2.304.906-4.189 3.024-4.189 1.247 0 2.57.828 2.684 2.411.123 1.666-.767 2.511-1.892 3.582-2.924 2.78-2.816 4.049-2.816 7.196h3.943c0-1.452-.157-2.508 1.838-4.659 1.331-1.436 2.986-3.222 3.021-5.943.047-3.963-2.751-6.398-6.697-6.398z"/></svg>Découvrir le projet</a></li>
+                <li class="intro__el"><a href="app.php" class="btn">Lancer l'aventure</a></li>
+            </ul>   
         </div>
         
     </section>
