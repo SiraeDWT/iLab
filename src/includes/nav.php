79d64d8b0401504209edf456c4f39fa7f6b3240f
Dylan <dylanv.dwt@hotmail.com>
2024-10-15 12:42:42 +0200

    Add menu

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -1,5 +1,9 @@
 <header>
-    <nav>
-        
+    <nav class="menu">
+        <ul class="menu__list">
+            <li class="menu__el"><a href="#">Présentation</a></li>
+            <li class="menu__el"><a href="#">Projet</a></li>
+            <li class="menu__el"><a href="#">Équipe</a></li>
+        </ul>
     </nav>
 </header>
