316d2d5aa392a2e523dc8edb566ace72bed504bb
Dylan <dylanv.dwt@hotmail.com>
2024-10-15 19:02:36 +0200

    Add sticky nav

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,6 +10,21 @@ import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 
+let oldScrollY = 0;
+const menu = document.querySelector('.header');
+
+window.addEventListener('scroll', scrollListener);
+
+function scrollListener() {
+    if(oldScrollY > window.scrollY){
+        menu.classList.remove('header--hide');
+    } else{
+        menu.classList.add('header--hide');
+    }
+    oldScrollY = window.scrollY;
+}
+
+
 let character = document.querySelector('.app__character');
 let message = document.querySelector('.app__message');
 let movementEnabled = true;
